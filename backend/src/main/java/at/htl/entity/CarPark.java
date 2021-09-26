75fdb0017efa88a367c824d9b601cacaabce7ba4
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-09-26 12:07:48 +0200

    Wiesinger | add variables

--- backend/src/main/java/at/htl/entity/CarPark.java
+++ backend/src/main/java/at/htl/entity/CarPark.java
@@ -1,4 +1,10 @@
 package at.htl.entity;
 
+import java.util.ArrayList;
+
 public class CarPark {
+    Long id;
+    Double[] location;
+    String name;
+    ArrayList<ParkingSpots> spots = new ArrayList<ParkingSpots>();
 }
