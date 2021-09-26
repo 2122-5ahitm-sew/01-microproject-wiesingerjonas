a7d5cde0e915c15c0dfee72192a8525d0e78efbc
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-09-26 12:10:27 +0200

    Wiesinger | add comments

--- backend/src/main/java/at/htl/entity/CarPark.java
+++ backend/src/main/java/at/htl/entity/CarPark.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class CarPark {
     Long id;
-    Double[] location;
-    String name;
-    ArrayList<ParkingSpots> spots = new ArrayList<ParkingSpots>();
+    Double[] location;  //location in decimal degrees
+    String name;        //name of the car park (example: "Garage Promenade Linz")
+    ArrayList<ParkingSpots> spots = new ArrayList<ParkingSpots>();  //List of all parking spots in the car park
 }
